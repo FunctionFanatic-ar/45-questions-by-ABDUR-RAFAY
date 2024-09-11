a963f169eb33f6c7caa4f2a1af327e88ba2e3474
FunctionFanatic-ar <arafayatif@gmail.com>
2024-09-12 01:19:50 +0500

    Delete main.ts

--- main.ts
+++
@@ -1,6 +0,0 @@
-let myname : string= 'abdur rafay'
-
-console.log(myname.toLowerCase())
-console.log(myname.toUpperCase())
-console.log(myname.charAt(0).toUpperCase() + myname.slice(1).toLowerCase() ) 
-
